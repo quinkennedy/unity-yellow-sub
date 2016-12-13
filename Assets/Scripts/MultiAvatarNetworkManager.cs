72bf4de6f3e95b2c67b304744d1323c93d2b2ff6
Quin Kennedy <quinkennedy@gmail.com>
2016-12-13 09:44:07 -0800

    different device types get different avatars

---
+++ Assets/Scripts/MultiAvatarNetworkManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MultiAvatarNetworkManager : NetworkManager {
+
+    //subclass for sending network messages
+    public class AvatarMessage : MessageBase
+    {
+        //default to index 0 which should be a "generic" avatar
+        public int avatarIndex = 0;
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId, NetworkReader extraMessageReader)
+    {
+        AvatarMessage message = extraMessageReader.ReadMessage<AvatarMessage>();
+        int selectedAvatar = message.avatarIndex;
+        Debug.Log("server add with message " + selectedAvatar);
+
+        GameObject player = Instantiate(spawnPrefabs[selectedAvatar]);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        AvatarMessage msg = new AvatarMessage();
+        for (int i = 0; i < spawnPrefabs.Count; i++)
+        {
+            if (spawnPrefabs[i].Equals(playerPrefab))
+            {
+                msg.avatarIndex = i;
+                break;
+            }
+        }
+
+        ClientScene.AddPlayer(conn, 0, msg);
+    }
+
+
+    public override void OnClientSceneChanged(NetworkConnection conn)
+    {
+        //base.OnClientSceneChanged(conn);
+    }
+}
