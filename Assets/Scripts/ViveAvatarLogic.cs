4e3870e1c6981b73c1375f00d63c08931fce53d5
Quin Kennedy <quinkennedy@gmail.com>
2016-12-14 09:02:01 -0800

    Mapping Vive to Hololens world-space
    Using a registration point at 0,0,0:
    Line up the center of the physical controller with the registration point.
    Tap the menu button on the physical controller.
    The virtual controller should now be mapped to the physical controller.
    
    Unfortunately the Coordinate spaces btw Vive and Hololens may be rotated
    so the further you get from the origin, the more the physical
    and virtual controllers drift apart.

--- Assets/Scripts/ViveAvatarLogic.cs
+++ Assets/Scripts/ViveAvatarLogic.cs
@@ -7,6 +7,8 @@ using UnityEngine.Networking;
 public class ViveAvatarLogic : NetworkBehaviour {
 
     private Transform _head, _rightController, _leftController;
+    private Vector3 _rightControllerOffset = Vector3.zero;
+    private Vector3 _leftControllerOffset = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
@@ -23,9 +25,21 @@ public class ViveAvatarLogic : NetworkBehaviour {
             {
                 _head.position = Camera.main.transform.position;
             }
-            _rightController.position = InputTracking.GetLocalPosition(VRNode.RightHand);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Debug.Log("[ViveAvatarLogic:Update] Fire1 pressed");
+                _rightControllerOffset = -InputTracking.GetLocalPosition(VRNode.RightHand);
+            }
+            _rightController.position = InputTracking.GetLocalPosition(VRNode.RightHand) + _rightControllerOffset;
             _rightController.rotation = InputTracking.GetLocalRotation(VRNode.RightHand);
-            _leftController.position = InputTracking.GetLocalPosition(VRNode.LeftHand);
+
+            if (Input.GetButtonDown("Fire2"))
+            {
+                Debug.Log("[ViveAvatarLogic:Update] Fire2 pressed");
+                _leftControllerOffset = -InputTracking.GetLocalPosition(VRNode.LeftHand);
+            }
+            _leftController.position = InputTracking.GetLocalPosition(VRNode.LeftHand) + _leftControllerOffset;
             _leftController.rotation = InputTracking.GetLocalRotation(VRNode.LeftHand);
         }
     }
